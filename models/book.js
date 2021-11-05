18c787cc734d11f723ae3df52aefdd21ab6f696c
Rohit Kumar Bisht <63202878+rohitkumarbisht@users.noreply.github.com>
2021-11-05 12:13:40 +0530

    Add files via upload

--- models/book.js
+++ models/book.js
@@ -1,9 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-let bookScheme = new mongoose.Schema({
-    name: String,
-    author: String,
-    price: Number
-});
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
+})
 
-module.exports = mongoose.model('Book', bookScheme);
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
